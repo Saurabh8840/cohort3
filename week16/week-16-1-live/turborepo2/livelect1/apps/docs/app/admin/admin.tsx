f22958254fa58ae78c9c1638e7eed9a7a895c116
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-11-25 23:46:53 +0530

    added to week16

---
+++ week16/week-16-1-live/turborepo2/livelect1/apps/docs/app/admin/admin.tsx
@@ -0,0 +1,12 @@
+
+import {Admin} from "@repo/ui/admin";
+import {InputBox} from "@repo/ui/input-Box";
+
+export default function (){
+
+    return <div>
+        hii from admin page
+        <Admin />
+        <InputBox />
+    </div>
+}
